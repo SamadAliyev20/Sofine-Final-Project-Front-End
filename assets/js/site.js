705c3e9d9198b859af6e7d6ec15474aadcb70a04
SamadAliyev20 <samadaa@code.edu.az>
2023-04-04 21:23:58 +0400

    improves

--- assets/js/site.js
+++ assets/js/site.js
@@ -34,4 +34,66 @@ $(document).ready(function(){
         }
       ]
     });
+    $('.product-slider').slick({
+      infinite: false,
+      speed: 600,
+      slidesToShow: 5,
+      slidesToScroll: 1,
+      swipe: true,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+           
+          }
+        },
+        {
+          breakpoint: 599,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            
+          }
+        },
+        {
+          breakpoint: 1649,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            swipe: true
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            swipe: true
+          }
+        }
+      ]
+    });
+    
   });
