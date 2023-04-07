c29855b245c36428d2152c830cfbe994c5936ac5
SamadAliyev20 <samadaa@code.edu.az>
2023-04-07 20:52:10 +0400

    brand section completed

--- assets/js/site.js
+++ assets/js/site.js
@@ -142,4 +142,50 @@ $(document).ready(function(){
         }
       ]
     });
+    $('.brand-carousel').slick({
+      infinite: false,
+      slidesToShow:6,
+      speed: 600,
+      arrows: false,
+      swipe: false,
+      settings: "unslick",
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: false,
+            swipe: true,
+            arrows: false,
+          }
+        },
+        {
+          breakpoint: 1400,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            infinite: false,
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            arrows: false,
+            slidesToScroll: 1,
+            swipe: true
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            arrows: false,
+            slidesToScroll: 1,
+            swipe: true
+          }
+        }
+      ]
+    });
   });
